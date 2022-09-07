35a336d485dfbabcf52b7db336cf6fcbb02c2e5e
PetreaBusk <petreabusk@gmail.com>
2022-09-07 16:44:25 +0200

    init

---
+++ src/Enemy.js
@@ -0,0 +1,38 @@
+import {gsap} from "gsap";
+import setRandomInterval from "set-random-interval";
+import * as PIXI from "pixi.js";
+import {spine} from "pixi-spine";
+
+class Enemy{
+constructor(obj){
+
+console.log("obj")
+
+
+
+// this.resname = items.name;
+// this.container = items.addTo;
+
+// this.startFrom = 0;
+// this.endAt = 0;
+// this.front = 0;
+// this.enemyArray= [];
+// this.enemyDuration = [20,21,22,23,24,25,26,27,28,29,30,40,50];
+
+// this.from = ["left", "right"];
+
+// this.counter=0;
+
+
+
+
+
+
+
+
+
+
+}//constructor END
+}//class END
+
+export default Enemy;
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import Game from "./Game";
+import "../css/style.scss";
+
+
+// **  IIFE: Immediately Invoked Function Expression  */
+
+(function () {
+
+  // console.log("starting the cool->app");
+
+  let game = new Game();
+
+})();
